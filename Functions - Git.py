f8db5e3aa81bae5df51d48759f6bb3bde4501584
Sophia Carahaly <02sophia.carahaly@gulllakecs.org>
2019-02-14 09:18:06 -0500

    Add a Greetings Function

---
+++ Functions - Git.py
@@ -0,0 +1,12 @@
+# 4.13.3 : Greeting
+# Sophia Carahaly
+# 2.6.19
+
+name = input("What is your name: ")
+
+def greeting():
+    print ("Hi there " + name + "!")
+    print ("Nice to meet you!")
+
+greeting()
+
