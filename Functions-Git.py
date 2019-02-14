34ae29e8a03df72dbd3d9e2b76cb686a871a16ee
Sophia Carahaly <02sophia.carahaly@gulllakecs.org>
2019-02-14 09:51:41 -0500

    Add Functions and Variables

---
+++ Functions-Git.py
@@ -0,0 +1,27 @@
+# 4.13.3 : Greeting
+# Sophia Carahaly
+# 2.6.19
+'''
+name = input("What is your name: ")
+
+def greeting():
+    print ("Hi there " + name + "!")
+    print ("Nice to meet you!")
+
+greeting()
+'''
+
+# 4.13.4 : Functions and Variables
+# Sophia Carahaly
+# 2.14.19
+
+x = 42
+
+
+def print_something():
+    x = 13
+    print(x)
+
+
+print_something()
+print (x)
