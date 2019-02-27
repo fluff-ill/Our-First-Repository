6c7669463a2d5c55607d5cc732801d502c28c57f
Sophia Carahaly <02sophia.carahaly@gulllakecs.org>
2019-02-27 09:59:32 -0500

    Add Functions and Variables, Part 3

--- Functions-Git.py
+++ Functions-Git.py
@@ -1,7 +1,7 @@
 # 4.13.3 : Greeting
 # Sophia Carahaly
 # 2.6.19
-'''
+
 name = input("What is your name: ")
 
 def greeting():
@@ -9,13 +9,13 @@ def greeting():
     print ("Nice to meet you!")
 
 greeting()
-'''
+
 
 # 4.13.4 : Functions and Variables
 # Sophia Carahaly
 # 2.14.19
 
-x = 42
+x = 89
 
 
 def print_something():
@@ -27,7 +27,26 @@ print_something()
 print(x)
 
 
-# 4.13.4 : Functions and Variables: Part Two
+# 4.13.5 : Functions and Variables: Part Two
 # Sophia Carahaly
 # 2.14.19
 
+my_variable = 3.6745
+
+
+def something():
+    print(my_variable + 10)
+
+something()
+
+# 4.13.6: Functions and Variables, Part 3
+# Sophia Carahaly
+# 2.18.19
+
+
+def print_number(x):
+    print(str(x))
+
+
+print_number(12)
+print_number('\n' + 'Hello World')
