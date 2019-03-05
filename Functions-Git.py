41902a3ecfc72a28c4fa8c10681a83c888d119a5
Sophia Carahaly <02sophia.carahaly@gulllakecs.org>
2019-03-05 09:11:19 -0500

    Add Temperature Converter

--- Functions-Git.py
+++ Functions-Git.py
@@ -72,7 +72,7 @@ print_name_and_age("Marcus", 19)
 
 def print_two_numbers(x, y=20):
     print('First number:', x)
-      print('Second number: ' + str(y))
+    print('Second number: ' + str(y))
 
 
 print_two_numbers(3, 2)
@@ -130,3 +130,27 @@ try:
     print('Your number: ' + str(my_number))
 except ValueError:
     print('That was not an integer')
+
+
+# 4.16.9: Temperature Converter
+# Sophia Carahaly
+# 2.20.19
+
+
+def convert_cf(C):
+    return 1.8 * C + 32
+
+
+def convert_fc(F):
+    return (F - 32) / 1.8
+
+
+try:
+    c = float(input('Enter a temp in C: '))
+    print('In F: ', round(convert_cf(c), 2))
+
+    f = float(input('Enter a temp in F: '))
+    print('In F: ', round(convert_fc(f), 2))
+
+except ValueError:
+    print('You must enter a float')
